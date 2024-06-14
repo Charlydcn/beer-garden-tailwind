47047a70ce19d7eeb8cc1630b5befadaf8067764
Charlydcn <Charly.ducournau@outlook.fr>
2024-06-14 08:31:35 +0200

    more padding-y for mobile

--- src/components/articles/BigArticle.jsx
+++ src/components/articles/BigArticle.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export default function BigArticle({ className = null, title, smallText, text, btn}) {
     return (
-        <article className={`flex flex-col text-center max-w-[55rem] items-center py-8 min-h-[50svh] justify-center w-11/12 mx-auto leading-7 h-full ${className} sm:w-3/4 sm:aspect-square lg:min-h-0`}>
+        <article className={`flex flex-col text-center max-w-[55rem] items-center py-20 min-h-[50svh] justify-center w-11/12 mx-auto leading-7 h-full ${className} sm:w-3/4 sm:aspect-square lg:min-h-0`}>
             <h2 className='text-4xl mb-2 md:text-5xl'>{title}</h2>
             <p>
                 <small className='italic times-new text-base md:text-mg'>
