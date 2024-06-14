4a50c20b442f6c122fe358f0c8ca2f0372398ca5
Charlydcn <Charly.ducournau@outlook.fr>
2024-06-14 08:35:07 +0200

    removed hover for mobile

--- src/components/articles/SmallArticle.jsx
+++ src/components/articles/SmallArticle.jsx
@@ -5,9 +5,9 @@ function SmallArticle({ img, imgAlt = null, title, text, className = null}) {
         <article className={`group flex flex-col items-center w-80 max-w-[80vw] text-center ${className}`}>
             <div className="h-20 mb-4 relative">
                 <img className="w-full h-full object-scale-down" src={img} alt={imgAlt} />
-                <div className='group-hover:opacity-100 transition-opacity opacity-0 delay-150 duration-150 bg-gold w-full h-full absolute top-0 rounded-full mix-blend-darken'></div>
+                <div className='lg:group-hover:opacity-100 transition-opacity opacity-0 delay-150 duration-150 bg-gold w-full h-full absolute top-0 rounded-full mix-blend-darken'></div>
             </div>
-            <h4 className='font-bold group-hover:text-gold transition-colors duration-500'>{title}</h4>
+            <h4 className='font-bold lg:group-hover:text-gold transition-colors duration-500'>{title}</h4>
             <p className='opacity-50 raleway text-sm leading-6 font-semi-light'>{text}</p>
         </article>
     )
